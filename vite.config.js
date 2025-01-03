980b69e9513acfefa3304fcec27bf3f7e581f30d
kimmahan <kim@maxxpotential.com>
2025-01-03 16:18:06 -0500

    Simplify build

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      extensions: ['.js', '.cjs']
-    }
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [react()]
 })
