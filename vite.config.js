4d2a603a9638d3cea35da14c6bf3742c581fc4b4
kimmahan <kim@maxxpotential.com>
2025-01-03 16:09:58 -0500

    Adding Vercel config and update build settings

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks: undefined
-      }
+    commonjsOptions: {
+      include: [/node_modules/],
+      extensions: ['.js', '.cjs']
     }
   },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'recharts']
+  server: {
+    port: 3000
   }
 })
