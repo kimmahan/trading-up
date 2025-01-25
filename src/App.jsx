bb9dd23d03d146d955a52c2e1a09222e651c076b
kimmahan <kim@maxxpotential.com>
2025-01-25 16:53:24 -0500

    Adding Vercel analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // App.jsx
 import React from 'react';
 import EducationROICalculator from './components/EducationROICalculator';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <EducationROICalculator />
+      {/* <Analytics /> */}
     </div>
   );
 }
